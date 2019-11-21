cdfbcb6feeb74f03b4cc14c17e4d68190096b458
Murat J. Isidore <rmurat2@yahoo.fr>
2019-11-21 12:21:59 -0500

    Initial commit

---
+++ pluralsight-springcloud-m1-startup/src/main/java/pluralsight/demo/Application.java
@@ -0,0 +1,22 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
+	public String sayHello() {
+		return "Hello from spring";
+	}
+
+}
